1d524696e29c221075218a92c0322554faa5fedf
Amr Omran <amr.omran@check24.de>
2025-09-22 20:28:43 +0200

    Initial Commit

---
+++ VintageLens/VintageLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  VintageLensApp.swift
+//  VintageLens
+//
+//  Created by Amr Omran on 22.09.25.
+//
+
+import SwiftUI
+
+@main
+struct VintageLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VintageLensTests/VintageLensTests.swift
@@ -0,0 +1,17 @@
+//
+//  VintageLensTests.swift
+//  VintageLensTests
+//
+//  Created by Amr Omran on 22.09.25.
+//
+
+import Testing
+@testable import VintageLens
+
+struct VintageLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
